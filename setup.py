601a27c3322187e539d9b7cc00ff325dfea4813d
muladzevitali <muladzevitali@gmail.com>
2023-09-30 02:06:58 +0400

    add slack services

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Connectify",
+    version="0.1",
+    author="Vitali Muladze",
+    author_email="muladzevitali@gmail.com",
+    description="Different Python I/O handlers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/muladzevitali/connectify",
+    project_urls={
+        "Bug Tracker": "https://github.com/muladzevitali/connectify/issuesL",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8"
+)
