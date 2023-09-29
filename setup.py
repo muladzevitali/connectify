2fb5c0ab4c811d50943ead5002129be285096978
muladzevitali <muladzevitali@gmail.com>
2023-09-30 02:29:31 +0400

    update requirements, add dependencies in setup file

--- setup.py
+++ setup.py
@@ -3,13 +3,17 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
 setuptools.setup(
     name="Connectify",
-    version="0.1",
+    version="0.1.0",
     author="Vitali Muladze",
     author_email="muladzevitali@gmail.com",
     description="Different Python I/O handlers",
     long_description=long_description,
+    install_requires=install_requires,
     long_description_content_type="text/markdown",
     url="https://github.com/muladzevitali/connectify",
     project_urls={
