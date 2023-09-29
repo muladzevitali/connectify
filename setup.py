75e2f9fb8d3469f5f2c80fe50fdc8d76e1499e39
muladzevitali <muladzevitali@gmail.com>
2023-09-30 03:26:36 +0400

    update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="connectify",
-    version="0.1.0",
+    version="0.1.2",
     author="Vitali Muladze",
     author_email="muladzevitali@gmail.com",
     description="Different Python I/O handlers",
