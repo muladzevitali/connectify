122d0dc6e401a8b833b837bd52db21f9f3a94b1b
muladzevitali <muladzevitali@gmail.com>
2023-10-01 00:15:28 +0400

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="connectify",
-    version="0.1.3",
+    version="0.1.4",
     author="Vitali Muladze",
     author_email="muladzevitali@gmail.com",
     description="Different Python I/O handlers",
